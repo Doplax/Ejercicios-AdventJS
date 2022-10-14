c3c295496d3e4ef8c15721658375177e021c9c20
Doplax <doplax@gmail.com>
2022-10-14 11:34:40 +0200

    Haciendo pruebas

--- ej2_regalos.js
+++ ej2_regalos.js
@@ -1,1 +1,29 @@
+function print(element){ console.log(element);} 
 const carta = 'bici coche balón _playstation bici coche peluche'
+
+const lista = carta.split(" ");
+
+
+let objeto = {}
+
+lista.forEach((Element,i) => {objeto[Element] = 0});
+
+lista.sort() 
+
+delete lista[0]
+
+for (let i of lista) {
+    print(lista)
+}
+
+// objeto["bici"] = 1
+// if (objeto["bici"]){
+//     console.log("Cierto");
+// } else {
+//     console.log("No ha entrado");
+// }
+
+
+// if (lista[i].charAt(0) === "_") {
+    
+// }
