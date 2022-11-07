9adec3b9280c5a801612d73211a6f17cf7c228e7
Doplax <doplax@gmail.com>
2022-11-07 09:04:59 +0100

    Empezando el ejercicio 3

--- ej1_contar.js
+++ ej1_contar.js
@@ -7,5 +7,8 @@ const ovejas = [
     { name: 'Nnnnnnnn', color: 'rojo'}
   ]
 
+  ovejas_filtradas =  ovejas.filter(oveja =>  oveja.color === "rojo"   &&                               // Todas las que no sean rojas seran descartadas. 
+                                                                    oveja.name.toLowerCase().includes("n")  &&    
+                                                                    oveja.name.toLowerCase().includes("a"))           // Para filtrar las que tengan una 'n' && 'a' en su nombre
 
-  
+  console.log(ovejas_filtradas);
--- ej2_regalos.js
+++ ej2_regalos.js
@@ -1,26 +1,31 @@
 const carta = 'bici coche balón _playstation bici coche peluche'
 
-const lista = carta.split(" ").sort();
-
-let objeto = {}
-
-console.log(objeto);
 
 
+function carta_order(list) {
+                return list
+                // .trim() // Quita los espacios en blancos del principio y del final
+                .split(" ")// Crea una lista separando los elementos que tengan un espacio entre ellos en este caso
+                .filter(Element => !Element.startsWith("_")) // Para que saque todos los elementos menos los que empiecen con "_"
+                .reduce((account,gift) => { // No entiendo esta parte
+                    account[gift] = account[gift] ? account[gift] + 1 : 1
+                    return account},{}) // No entiendo la coma y el  {} 
+}
 
-// for (let i of lista) {
-//     objeto[i]
-//     console.log(i)
-// }
+let lista = carta_order(carta)
+console.log(lista);
+ 
 
 
 
-// objeto["bici"] = 1
-// if (objeto["bici"]){
-//     console.log("Cierto");
-// } else {
-//     console.log("No ha entrado");
-// }
 
-
-// if (lista[i].charAt(0) === "_") { lista[i].slice  }
+// export default function listGifts(letter) {
+//     return letter
+//       .trim()
+//       .split(/\s+/)
+//       .filter(gift => !gift.startsWith('_'))
+//       .reduce((acc, gift) => {
+//         acc[gift] = acc[gift] ? acc[gift] + 1 : 1
+//         return acc
+//       }, {})
+//   }
